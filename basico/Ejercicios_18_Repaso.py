e9a4416ff03afe505ee078eb50c51531daa0b606
IsabelManiega <isabelmaniega@hotmail.com>
2022-07-21 10:03:21 +0200

    añadido requerimientos y ejercicio 4 /5

--- basico/Ejercicios_18_Repaso.py
+++ basico/Ejercicios_18_Repaso.py
@@ -24,8 +24,8 @@ print(maximo(8, 5))
 
 
 """
-    Definir una función max_de_tres(), 
-    que tome tres números como argumentos y 
+    Definir una función max_de_tres(),
+    que tome tres números como argumentos y
     devuelva el mayor de ellos.
 """
 
@@ -38,13 +38,37 @@ print(max_de_tres())
 # EJERCICIO 3
 
 """
-    Definir una función que calcule la longitud de una lista 
-    o una cadena dada. 
-    (Es cierto que python tiene la función len() incorporada, 
+    Definir una función que calcule la longitud de una lista
+    o una cadena dada.
+    (Es cierto que python tiene la función len() incorporada,
     pero escribirla por nosotros mismos resulta un muy buen ejercicio.
 """
 
 def longitud():
     pass
+
+# EJERCICIO 4
+
+"""
+    Escribir una función que tome un carácter y devuelva True si es una vocal,
+    de lo contrario devuelve False.
+"""_
+
+def es_vocal():
+    pass
+
+# EJERCICIO 5
+
+"""
+    Escribir una función suma() y una función multip() que sumen y multipliquen
+    respectivamente todos los números de una lista.
+    Por ejemplo: sum([1,2,3,4]) debería devolver 10 y multip([1,2,3,4]) debería devolver 24.
+"""
+def suma():
+    pass
+
+def multip():
+    pass
+
 
 print("Enhorabuena acabaste los ejercicios")
